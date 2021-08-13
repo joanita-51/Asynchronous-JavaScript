70c3923e58812d22d9a2cb17b8b1b0c72d1289e9
JOANITA-51 <nakityojoanita51@gmail.com>
2021-08-13 16:48:54 +0300

    Showing the synchronous nature of javaScript # execution of the program one after the other

--- node-crash-course/10_fs_sync.js
+++ node-crash-course/10_fs_sync.js
@@ -1,12 +1,13 @@
+const { Console } = require('console')
 const {readFileSync, writeFileSync}= require('fs')//the destructuring method. similar to const fs = require('fs')
-
+console.log('Start')
 const first = readFileSync('./content/first.txt','utf-8')
 const second = readFileSync('./content/second.txt','utf-8')
-
-console.log(first, second)
 
 writeFileSync(
     './content/result-sync.txt',
     `Here is the result : ${first}, ${second}`,
     {flag :'a'}
 )
+console.log('done with this task')
+console.log('starting the next one')
