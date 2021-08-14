2d416bacfdc20805f4fa6d20e7f7e3c3f4c3df56
JOANITA-51 <nakityojoanita51@gmail.com>
2021-08-14 18:28:57 +0300

    navigating through the url property in the request object

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -1,7 +1,20 @@
 const http = require('http')
 
 const server = http.createServer((request, response)=>{
-response.write('Welcome to our home page')
-response.end()
+    if (request.url === '/') {
+        response.write('Welcome to our home page')
+    }
+    else if (request.url === '/about') {
+        response.write('Here is our short history')
+    }
+    else {
+        response.write(`
+        <h1>Oops!</h1>
+        <p>We couldn't find what you are looking for </p>
+        <a href ='/'> back home </a>
+        `)
+    }
+    response.end()
+    
 })
 server.listen(5000)
