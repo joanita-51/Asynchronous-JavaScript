bb0761a61bd64608b6ea6c3a71feed1f8ad5e0ba
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-04 19:42:28 +0300

    modifications and asynchronous in node JS

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -1,20 +1,32 @@
-const http = require('http')
+const { readFile , writeFile } =require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const server = http.createServer((request, response)=>{
-    if (request.url === '/') {
-        response.write('Welcome to our home page')
+const start = async()=> {
+    try {
+       const first = await readFile('./content/first.txt','utf8') 
+       const second = await readFile('./content/second.txt', 'utf8') 
+       await writeFile('./content/result-mind-grenade.txt', 'THIS IS AWESOME : 2+2 = 4', {flag:'a'})
+       console.log (first, second)
+    } catch (error) {
+        console.log(error)
     }
-    else if (request.url === '/about') {
-        response.write('Here is our short history')
-    }
-    else {
-        response.write(`
-        <h1>Oops!</h1>
-        <p>We couldn't find what you are looking for </p>
-        <a href ='/'> back home </a>
-        `)
-    }
-    response.end()
-    
-})
-server.listen(5000)
+}
+start()
+
+// const getText = (path) =>{
+//     return new Promise((resolve, reject)=>{
+//         readFile (path, 'utf8', (err, data)=>{
+//             if (err){
+//                 reject (err)
+//             } else {
+//                 resolve (data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+//  .then ((result) => console.log(result))
+//  .catch((err) => console.log(err))
