6780e7fc413868d830d567a2a8b3aafefd1c856c
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-04 20:07:55 +0300

    adding parameters

--- node-crash-course/events.js
+++ node-crash-course/events.js
@@ -3,8 +3,8 @@ const EventEmitter = require('events')
 
 const customEmitter = new EventEmitter
 
-customEmitter.on ('response', ()=>{
-    console.log('data received')
+customEmitter.on ('response', (name, id)=>{
+    console.log(`data received !! User ${name} with id ${id}`)
 })
 
-customEmitter.emit('response')
+customEmitter.emit('response', 'Joanita', 9)
